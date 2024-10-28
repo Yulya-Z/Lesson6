15ce9ffb2b5485e216b6c5d0077df895d9b91923
Yulya-Z <zukovayulya@gmail.com>
2024-10-28 15:00:43 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -3,6 +3,13 @@ class Animal:
     fed = False     # (накормленный)
     def __init__(self, name):
         self.name = name
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
 
 class Plant:
     edible = False  # (съедобность)
@@ -10,29 +17,16 @@ class Plant:
         self.name = name
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
-class Predator(Animal):
+    pass
 
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+class Predator(Animal):
+    pass
 
 class Flower(Plant):
     pass
 
 class Fruit(Plant):
     edible = True
-
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
